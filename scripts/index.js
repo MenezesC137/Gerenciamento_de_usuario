fb5a350398266b653950007c04b286ab93984d47
Carlos <iamcarloseduardo@hotmail.com>
2022-03-02 13:54:37 -0300

    Events

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,20 @@
-var name = document.querySelector('#exampleInputName');
-var gender = document.querySelectorAll('#form-user-create [name=gender]:checked');
-var birth = document.querySelector('#exampleInputBirth');
-var country = document.querySelector('#exampleInputCountry');
-var email = document.querySelector('#exampleInputEmail');
-var passworld = document.querySelector('#exampleInputPassword');
-var photo = document.querySelector('#exampleInputFile');
-var admin = document.querySelector('#exampleInputAdmin');
-
-
-
 var fields = document.querySelectorAll('#form-user-create [name]');
+var user = {};
 
 fields.forEach(function(field, index){
 
     if (field.name == 'gender'){
 
         if (field.checked) {
-            console.log('SIM', field);
+            user[field.name] = field.Value;
         }
 
     } else {
-
-        console.log('NÃO');
+        
+        user[field.name] = field.Value;
 
     }
 
-    //console.log(field.id, field.name, field.value, field.ariaChecked, index);
-
 });
+
+console.log(user);
