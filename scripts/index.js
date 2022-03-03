752f7bda90b5e226c2d30ae9ed79adb7820b67a2
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-03 09:38:43 -0300

    Objetos

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,26 @@
 var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
-console.log(user);
+function addLine(dateUser){
+
+    var tr = document.createElement('tr');
+
+    tr.innerHTML = 
+    `<tr>
+            <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
+            <td>${dateUser.name}</td>
+            <td>${dateUser.email}</td>
+            <td>${dateUser.admin}</td>
+            <td>${dateUser.birth}</td>
+            <td>
+                <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
+                <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
+            </td>
+    </tr>`;
+
+    document.getElementById('table-users').appendChild(tr);
+
+}
 
 document.getElementById('form-user-create').addEventListener('submit', function(event){
 
@@ -23,6 +42,6 @@ document.getElementById('form-user-create').addEventListener('submit', function(
     
     });
 
-    console.log(user);
+    addLine(user);
     
 });
