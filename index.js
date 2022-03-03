e6b0694659674e71ce47bdce482461db0a7fda66
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-03 10:12:32 -0300

    MVC

--- index.js
+++ index.js
@@ -1,24 +1,22 @@
 var fields = document.querySelectorAll('#form-user-create [name]');
 var user = {};
 
-function addLine(dateUser){
+function addLine(dataUser){
 
-    var tr = document.createElement('tr');
+    console.log(dataUser);
 
-    tr.innerHTML = 
+    document.getElementById('table-users').innerHTML =
     `<tr>
             <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
-            <td>${dateUser.name}</td>
-            <td>${dateUser.email}</td>
-            <td>${dateUser.admin}</td>
-            <td>${dateUser.birth}</td>
+            <td>${dataUser.name}</td>
+            <td>${dataUser.email}</td>
+            <td>${dataUser.admin}</td>
+            <td>${dataUser.birth}</td>
             <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
             </td>
     </tr>`;
-
-    document.getElementById('table-users').appendChild(tr);
 
 }
 
@@ -42,6 +40,17 @@ document.getElementById('form-user-create').addEventListener('submit', function(
     
     });
 
-    addLine(user);
+    var objectUser = new User(
+        user.name, 
+        user.gender, 
+        user.birth, 
+        user.country, 
+        user.email, 
+        user.password, 
+        user.photo, 
+        user.admin
+    );
+
+    addLine(objectUser);
     
 });
