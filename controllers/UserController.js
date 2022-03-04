16e99f9c215edfc72839573d17e1af8eb7da4797
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-04 08:56:18 -0300

    dataSet

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -84,9 +84,16 @@ class UserController {
     getValues(){
 
         let user = {};
+        let isValid = true;
 
         [...this.formEl.elements].forEach(function(field, index){
+        
+            if(['name', 'email', 'password'].indexOf(field.name) > -1 && !field.value) {
 
+                field.parentElement.classList.add('has-error');
+                isValid = false;
+
+            }
             if (field.name == 'gender'){
         
                 if (field.checked) {
@@ -104,6 +111,11 @@ class UserController {
             }
         
         });
+
+        if (!isValid){
+            return false;
+
+        }
     
         return new User(
             user.name, 
