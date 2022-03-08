c09ef05507e64d2ff92ffa77c4ed86b31bac202d
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-03-08 09:24:00 -0300

    Excluindo

--- models/User.js
+++ models/User.js
@@ -91,11 +91,15 @@ class User{
 
     getNewId(){
 
-        if (!window.id) window.id = 0;
+        let usersID = parseInt(localStorage.getItem("usersID"));
 
-        id++;
+        if (!usersID > 0) usersID = 0;
 
-        return id;
+        usersID++;
+
+        localStorage.setItem("usersID", usersID);
+
+        return usersID;
 
     }
 
@@ -124,6 +128,24 @@ class User{
             users.push(this);
 
         }   
+
+        localStorage.setItem("users", JSON.stringify(users));
+
+    }
+
+    remove(){
+
+        let users = User.getUsersStorage();
+
+        users.forEach((userData, index) => {
+
+            if (this._id == userData._id) {
+
+                users.splice(index, 1);
+
+            }
+
+        });
 
         localStorage.setItem("users", JSON.stringify(users));
 
